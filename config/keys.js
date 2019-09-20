df44df8be205b4740c778e107419280a7f638d33
Sophia <sophierithous@gmail.com>
2019-09-20 17:32:47 +0100

    deleted unwanted file

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-dbPassword =
-  "mongodb+srv://sophi-e:" +
-  encodeURIComponent("enakpoya1990") +
-  "@cluster0-qxyv5.mongodb.net/test?retryWrites=true&w=majority";
-
-module.exports = {
-  mongoURI: dbPassword
-};
