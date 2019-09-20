5d57201324f7de986b3649a930d291061c9be6fc
Sophia <sophierithous@gmail.com>
2019-09-20 23:35:26 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 dbPassword =
-  "mongodb+srv://sophi-e:" +
-  encodeURIComponent("enakpoya1990") +
+  "mongodb+srv://username:" +
+  encodeURIComponent("password") +
   "@cluster0-qxyv5.mongodb.net/test?retryWrites=true&w=majority";
 
 module.exports = {
