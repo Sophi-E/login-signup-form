eb78f20fae8ba3b1dc9c428e2c6876ffcc7f5e21
sophi-e <sophierithous@gmail.com>
2019-09-20 23:24:54 +0100

    added file

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+dbPassword =
+  "mongodb+srv://sophi-e:" +
+  encodeURIComponent("enakpoya1990") +
+  "@cluster0-qxyv5.mongodb.net/test?retryWrites=true&w=majority";
+
+module.exports = {
+  mongoURI: dbPassword
+};
